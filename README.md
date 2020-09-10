8707da3de190e2ffaf3e58d0d48dfc7127b872cb
mikoto2000 <mikoto2000@gmail.com>
2020-09-11 05:49:28 +0900

    Added project template.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spring Boot Study
+
+TODO
+
+# Docker イメージ
+
+- amazoncorretto:8u265
+    - [amazoncorretto - Docker Hub](https://hub.docker.com/_/amazoncorretto)
+- mysql:8.0.21
+    - [mysql - Docker Hub](https://hub.docker.com/_/mysql)
+
---
+++ project/src/test/java/dev/mikoto2000/study/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
