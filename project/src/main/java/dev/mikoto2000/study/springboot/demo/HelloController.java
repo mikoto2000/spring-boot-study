ed030c0e37a244784958788707d338e11faaa32e
mikoto2000 <mikoto2000@gmail.com>
2020-09-12 12:54:43 +0900

    "/hello" で "Hello, World!" テキストを返却するコントローラーを追加。

---
+++ project/src/main/java/dev/mikoto2000/study/springboot/demo/HelloController.java
@@ -0,0 +1,16 @@
+package dev.mikoto2000.study.springboot.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(Model model) {
+        return "Hello, World!";
+    }
+}
