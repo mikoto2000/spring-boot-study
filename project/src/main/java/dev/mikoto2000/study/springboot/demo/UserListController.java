6d65e2a23e450a712c3ed01eff8d593bc431a67f
mikoto2000 <mikoto2000@gmail.com>
2020-09-12 22:04:16 +0900

    タイプセーフソートを利用したソートに変更。

--- project/src/main/java/dev/mikoto2000/study/springboot/demo/UserListController.java
+++ project/src/main/java/dev/mikoto2000/study/springboot/demo/UserListController.java
@@ -1,6 +1,8 @@
 package dev.mikoto2000.study.springboot.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort.TypedSort;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,10 @@ public class UserListController {
     @GetMapping("/users")
     public String users(Model model) {
 
-        Iterable<User> users = userRepository.findAllByOrderByIdAsc();
+        TypedSort<User> user = Sort.sort(User.class);
+        Sort sort = user.by(User::getId).ascending();
+
+        Iterable<User> users = userRepository.findAll(sort);
 
         model.addAttribute("users", users);
 
--- project/src/main/java/dev/mikoto2000/study/springboot/demo/UserRepository.java
+++ project/src/main/java/dev/mikoto2000/study/springboot/demo/UserRepository.java
@@ -2,8 +2,9 @@ package dev.mikoto2000.study.springboot.demo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findAllByOrderByIdAsc();
+    List<User> findAll(Sort sort);
 }
